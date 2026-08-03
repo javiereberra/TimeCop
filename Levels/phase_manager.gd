69103b376b2a3640b5f9238f6f2682d395ce2f18
javiereberra <javier_berra@live.com>
2026-08-03 09:16:35 -0300

    Cambio de fase-timeline

--- Levels/phase_manager.gd
+++ Levels/phase_manager.gd
@@ -1,4 +1,25 @@
 extends Node
 
+# referencias las timelines
+@onready var timeline_a: Node2D = $"../Floor01/TimelineA"
+@onready var timeline_b: Node2D = $"../Floor01/TimelineB"
+# variable para timeline activa
+var timeline_a_active: bool = true
+
+# referencia al jugador
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+
 func change_phase() -> void:
 	print("Cambio de fase solicitado")
+	#cambiar timeline activa
+	timeline_a_active = not timeline_a_active
+	
+	#cambiar visibilidad
+	timeline_a.visible = timeline_a_active
+	timeline_b.visible = not timeline_a_active
+	
+	#cambiar capas de colisiòn del player
+	if timeline_a_active:
+		player.collision_mask = 1
+	else:
+		player.collision_mask = 2
