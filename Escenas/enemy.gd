e4d96d2d6aaf796530fb4fa3ff2805a87e82902e
javiereberra <javier_berra@live.com>
2026-08-13 22:47:14 -0300

    IA básica en enemigos

--- Escenas/enemy.gd
+++ Escenas/enemy.gd
@@ -1,5 +1,21 @@
 extends CharacterBody2D
 
+@export var speed := 100.0
+
+@onready var obstacle_check: RayCast2D = $ObstacleCheck
+
+func _ready():
+	obstacle_check.collision_mask = collision_layer
+	
+func _physics_process(_delta):
+	# Si hay una pared delante, gira 90 grados.
+	if obstacle_check.is_colliding():
+		rotation += deg_to_rad(90)
+		
+	# Avanza siempre hacia delante.
+	velocity = transform.x * speed	
+	move_and_slide()
+	
 # Elinimar al enemigo
 func die():
 	queue_free()
