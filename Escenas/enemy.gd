4219b81d270d9132b081f4b6b58e94935cc1789c
javiereberra <javier_berra@live.com>
2026-08-16 11:27:41 -0300

    Patrol básico de enemgo completo

--- Escenas/enemy.gd
+++ Escenas/enemy.gd
@@ -4,12 +4,15 @@ extends CharacterBody2D
 
 @onready var obstacle_check: RayCast2D = $ObstacleCheck
 
+@onready var patrol_timer: Timer = $PatrolTimer
+
 #Lista de Estados
 enum State {
 	PATROL
 }
 
 var current_state = State.PATROL
+var is_waiting := false
 
 func _ready():
 	obstacle_check.collision_mask = collision_layer
@@ -19,15 +22,26 @@ func _physics_process(_delta):
 		patrol()
 	
 func patrol():
-	# Si hay una pared delante, gira 90 grados.
-	if obstacle_check.is_colliding():
+	# Si hay una pared delante y no espera, gira 90 grados.
+	if obstacle_check.is_colliding() and not is_waiting:
 		rotation += deg_to_rad(90)
+		# espera, inicia timer
+		is_waiting = true
+		patrol_timer.start()
 		
-	# Avanza siempre hacia delante.
-	velocity = transform.x * speed	
+	# Avanza hacia delante siempre que no espere.
+	if is_waiting:
+		velocity = Vector2.ZERO
+	else:
+		velocity = transform.x * speed	
+		
 	move_and_slide()
 	
 # Elinimar al enemigo
 func die():
 	queue_free()
 	
+
+# cuando el timer termina, deja de esperar
+func _on_patrol_timer_timeout():
+	is_waiting = false
