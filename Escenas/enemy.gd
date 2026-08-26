160ce5d2ccbb30a0c49def689036c3c575221dc6
javiereberra <javier_berra@live.com>
2026-08-26 20:20:15 -0300

    IA Enemigos (parte 1)

--- Escenas/enemy.gd
+++ Escenas/enemy.gd
@@ -1,26 +1,38 @@
 extends CharacterBody2D
 
+# VARIABLES EXPORTABLES
 @export var speed := 100.0
-
+# REFERENCIAS A NODOS
 @onready var obstacle_check: RayCast2D = $ObstacleCheck
-
 @onready var patrol_timer: Timer = $PatrolTimer
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+# VARIABLES DE DETECCIÓN
+@export var vision_distance := 500.0
+@export var vision_angle := 90.0
 
-#Lista de Estados
+#  ---ESTADOS DE LA IA----
 enum State {
-	PATROL
+	PATROL,
+	ALERT
 }
 
 var current_state = State.PATROL
+
+# Variable de PATROL
 var is_waiting := false
 
+#Asigna capa de colisión correspondiente al detector de paredes
 func _ready():
 	obstacle_check.collision_mask = collision_layer
-	
+
+# PROCESAR LOS ESTADOS
 func _physics_process(_delta):
 	if current_state == State.PATROL:
 		patrol()
+	elif current_state == State.ALERT:
+		alert()
 	
+# FUNCIÒN DEL ESTADO PATROL
 func patrol():
 	# Si hay una pared delante y no espera, gira 90 grados.
 	if obstacle_check.is_colliding() and not is_waiting:
@@ -36,6 +48,18 @@ func patrol():
 		velocity = transform.x * speed	
 		
 	move_and_slide()
+
+#   ESTADOALERT  
+func alert():
+	pass
+	
+func can_see_player():
+	var distance_to_player := global_position.distance_to(player.global_position)
+	
+	if distance_to_player > vision_distance:
+		return false
+	
+	return true
 	
 # Elinimar al enemigo
 func die():
